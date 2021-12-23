f77cdced9e2400dffa71d80120bcd37cfdd8c67f
xaveerarif <xaveerarif@gmail.com>
2021-12-23 18:48:38 +0600

    remove banner

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import Footer from '../../Shared/Footer/Footer';
-import Navigation from '../../Shared/Navigation';
-import Banner from '../Banner/Banner';
-import Popular from '../Popular/Popular';
-import Services from '../Services/Services';
-import Comment from '../Comment/Comment'
+import React from "react";
+import Footer from "../../Shared/Footer/Footer";
+import Navigation from "../../Shared/Navigation";
+import Banner from "../Banner/Banner";
+import Popular from "../Popular/Popular";
+import Services from "../Services/Services";
+import Comment from "../Comment/Comment";
 
 const Home = () => {
-    return (
-        <div>
-            <Navigation/>
-            <Banner></Banner>
-            <Services></Services>
-            <Popular></Popular>
-            <Comment></Comment>
-            <Footer></Footer>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Navigation />
+      {/* <Banner></Banner> */}
+      <Services></Services>
+      <Popular></Popular>
+      <Comment></Comment>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
