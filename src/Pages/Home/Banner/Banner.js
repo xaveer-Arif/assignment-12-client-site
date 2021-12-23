37b4653eb007d05a58f8d20d620944feecb14ae6
xaveerarif <xaveerarif@gmail.com>
2021-12-23 18:22:11 +0600

    banner image change

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import { Carousel } from 'react-bootstrap';
-
+import React from "react";
+import { Carousel } from "react-bootstrap";
 
 const Banner = () => {
-    return (
-        <div>
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                     className="d-block w-100"
-                     src='https://i.ibb.co/XYjwwhq/5324809.jpg'
-                      alt="First slide"
-                      />
-
-                </Carousel.Item>
-            
-            </Carousel>
-        </div>
-    );
+  return (
+    <div>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://image.freepik.com/free-photo/young-woman-listening-music-earphones-her-appartment_1303-29275.jpg"
+            alt="First slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
